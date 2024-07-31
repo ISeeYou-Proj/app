4262837626f4ce20ab9160d3009287d5faa3215a
NARARIA03 <hyeonseong0305@gmail.com>
2024-07-31 21:33:25 +0900

    feat: uploadImagePage /voice 엔드포인트 연결 완료
    사진을 업로드해서 답변을 받은 뒤, 궁금한 점이 있다면
    볼륨 업 버튼을 누르고 말한 뒤 다시 볼륨 업 버튼을 누르고 기다리면
    AI가 추가적인 답변을 진행하는 기능 연결 완료

--- src/pages/Uploadimagepage.tsx
+++ src/pages/Uploadimagepage.tsx
@@ -4,11 +4,18 @@ import {Text, View, Image, TouchableOpacity} from 'react-native';
 import Sound from 'react-native-sound';
 import ImagePicker from 'react-native-image-crop-picker';
 import {API_URL} from '../utils/apiurl';
+import {useIsFocused} from '@react-navigation/native';
+import {useVolumeUpDown} from '../hooks/usevolumeupdown';
+import {useStt} from '../hooks/usestt';
 
 export default function UploadImagePage(): React.JSX.Element {
+  const isUploadImagePageActive = useIsFocused();
   const [prevBase64Img, setPrevBase64Img] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [aiResponse, setAiResponse] = useState<string>('');
+  const {volumeBtnState, resetVolumeState} = useVolumeUpDown({
+    initialVolume: 0.5,
+  });
 
   const getImage = () => {
     ImagePicker.openPicker({
@@ -61,11 +68,31 @@ export default function UploadImagePage(): React.JSX.Element {
       });
   };
 
+  // 볼륨 업버튼 클릭 시 녹음 시작하고, 다시 클릭 시 녹음 종료하는 훅, 서버로 post 요청을 하고 결과도 반환
+  const {recognizedText, isRecording} = useStt({
+    isActive: isUploadImagePageActive,
+    prevAnswer: aiResponse,
+    prevBase64Img: prevBase64Img,
+    setPrevAnswer: setAiResponse,
+    volumeBtnState: volumeBtnState,
+    resetVolumeState: resetVolumeState,
+  });
+
   return (
     <View className="w-full h-full flex justify-center items-center">
       <Text className="text-xl text-custom-black">
         업로드 이미지 페이지 입니다.
       </Text>
+      <Text className="text-xl text-custom-black">
+        녹음중?: {JSON.stringify(isRecording)}
+      </Text>
+      {isRecording ? (
+        <Text className="text-xl text-custom-black">
+          녹음중: {recognizedText}
+        </Text>
+      ) : (
+        <View />
+      )}
       {prevBase64Img === '' ? (
         <View />
       ) : (
