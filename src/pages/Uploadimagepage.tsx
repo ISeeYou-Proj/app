1145c3788572afce01b285f14b1ab1aee3ba008c
NARARIA03 <hyeonseong0305@gmail.com>
2024-07-31 15:28:29 +0900

    feat: 갤러리에서 이미지 가져오는 로직 구현
    버튼을 누르면 갤러리가 열리고, 이미지를 선택하면 해당 이미지의 base64
    를 가져와서 띄우는 것까지 구현 완료

--- src/pages/Uploadimagepage.tsx
+++ src/pages/Uploadimagepage.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import {Text} from 'react-native';
-
+import React, {useState} from 'react';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 export default function UploadImagePage(): React.JSX.Element {
+  const [imgBase64, setImgBase64] = useState('');
+
+  const getImage = () => {
+    ImagePicker.openPicker({
+      width: 200,
+      height: 300,
+      cropping: false,
+      includeBase64: true,
+      compressImageQuality: 0.6,
+      mediaType: 'photo',
+    })
+      .then(image => {
+        console.log('selected image name: ', image.filename);
+        setImgBase64(`data:${image.mime};base64,${image.data}`);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
-    <Text className="text-xl text-custom-black">
-      업로드 이미지 페이지 입니다.
-    </Text>
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-xl text-custom-black">
+        업로드 이미지 페이지 입니다.
+      </Text>
+      <Image
+        source={{uri: imgBase64}}
+        resizeMode="center"
+        className="w-48 h-48"
+      />
+      <TouchableOpacity
+        onPress={getImage}
+        className="w-48 p-4 m-4 bg-custom-blue items-center rounded-2xl">
+        <Text className="text-custom-white">이미지 가져오기</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
