058710fde7cff7ae2162e98874cf71f70912a3a1
NARARIA03 <hyeonseong0305@gmail.com>
2024-07-31 13:51:43 +0900

    feat: 업로드 이미지 페이지 뼈대 구성
    페이지 파일 생성 & BottomTabNav에 연결

---
+++ src/pages/Uploadimagepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function UploadImagePage(): React.JSX.Element {
+  return (
+    <Text className="text-xl text-custom-black">
+      업로드 이미지 페이지 입니다.
+    </Text>
+  );
+}
