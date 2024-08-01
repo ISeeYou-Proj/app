3b6fe78626a7a6897216914d44823d2484876436
NARARIA03 <hyeonseong0305@gmail.com>
2024-08-01 11:33:33 +0900

    fix: CameraPage 카메라 촬영에서 볼륨 매니저 로직 제거

--- src/components/Shutter.tsx
+++ src/components/Shutter.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import {VolumeManager} from 'react-native-volume-manager';
+import React, {useRef, useState} from 'react';
 import {View, TouchableOpacity} from 'react-native';
+import {Camera} from 'react-native-vision-camera';
+import {useCameraPostImg} from '../hooks/usecamerapostimg';
 
-export default function Shutter(): React.JSX.Element {
-  const handleClickShutter = () => {
-    VolumeManager.getVolume().then(volume => {
-      console.log('currentVolume: ', volume.volume);
-      VolumeManager.setVolume(volume.volume - 0.2);
-    });
+interface Props {
+  cameraRef: null | React.RefObject<Camera>;
+  isCamPageActive: boolean;
+  // handleClickShutter: () => {};
+}
+
+export default function Shutter({
+  cameraRef,
+  isCamPageActive,
+}: Props): React.JSX.Element {
+  const isTakePhoto = useRef<boolean>(false);
+  const [imagePath, setImagePath] = useState<string>('');
+
+  const resetImgPath = () => {
+    setImagePath('');
   };
+
+  /**
+   * @description 사진촬영 버튼을 누르면, 사진을 찍고 imagePath를 업데이트하는 함수
+   */
+  const handleClickShutter = () => {
+    if (cameraRef?.current && isCamPageActive) {
+      isTakePhoto.current = true;
+      cameraRef.current
+        .takePhoto({flash: 'auto'})
+        .then(photo => {
+          setImagePath(photo.path);
+          console.log('photo path: ', photo.path);
+        })
+        .catch(e => {
+          console.error(e);
+        })
+        .finally(() => {
+          isTakePhoto.current = false;
+        });
+    }
+  };
+
+  const {response, setResponse, prevBase64Img} = useCameraPostImg({
+    imagePath,
+    resetImgPath,
+  });
 
   return (
     <View className="w-20 h-20 relative">
